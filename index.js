eb9ec5cb3535ca3500f43090b289adf444c64224
Alirh1 <alirhayem.1212@gmail.com>
2024-07-05 22:53:00 +0300

    added node-fetch graceful-fs cheerio to node_modules

---
+++ index.js
@@ -0,0 +1,11 @@
+import fetch from "node-fetch";
+import fs from "graceful-fs";
+import * as cheerio from "cheerio";
+
+async function getJobId(id){
+    try{
+
+    }catch(error){
+        
+    }
+}
